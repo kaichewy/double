48a5b682f2b42554206d36bc1e735b36fcfbf1d2
Dylan <dylanaw16@gmail.com>
2025-06-08 23:07:07 +0800

    added tab

---
+++ frontend/double/app/(tabs)/_layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Tabs} from 'expo-router';
+
+const TabLayout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="standouts" />
+      <Tabs.Screen name="likes" />
+      <Tabs.Screen name="matches" />
+      <Tabs.Screen name="account" />
+    </Tabs>
+  );
+};
+
+export default TabLayout;
